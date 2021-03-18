32b11f4c634d87d177c80a20047b0eabecb9ccf5
Andrew Vu <vuandre1@my.yorku.ca>
2021-03-17 21:54:42 -0400

    Updating FCFS

--- src/main/FCFS.java
+++ src/main/FCFS.java
@@ -1,76 +1,88 @@
 package main;
 
-import java.util.*;
-import java.text.ParseException; 
 /*
- * FCFS (Nonpreemptive First-Come, First-Served Scheduling)
+ * FCFS (Non-preemptive First-Come, First-Served Scheduling)
  */
 
 public class FCFS {
-	static void findWaitingTime(int processes[], int n, 
-            int bt[], int wt[]) { 
-        // waiting time for first process is 0  
-        wt[0] = 0; 
-  
-        // calculating waiting time  
-        for (int i = 1; i < n; i++) { 
-            wt[i] = bt[i - 1] + wt[i - 1]; 
-        } 
-    } 
-  
-    // Function to calculate turn around time  
-    static void findTurnAroundTime(int processes[], int n, 
-            int bt[], int wt[], int tat[]) { 
-        // calculating turnaround time by adding  
-        // bt[i] + wt[i]  
-        for (int i = 0; i < n; i++) { 
-            tat[i] = bt[i] + wt[i]; 
-        } 
-    } 
-  
-    //Function to calculate average time  
-    static void findavgTime(int processes[], int n, int bt[]) { 
-        int wt[] = new int[n], tat[] = new int[n]; 
-        int total_wt = 0, total_tat = 0; 
-  
-        //Function to find waiting time of all processes  
-        findWaitingTime(processes, n, bt, wt); 
-  
-        //Function to find turn around time for all processes  
-        findTurnAroundTime(processes, n, bt, wt, tat); 
-  
-        //Display processes along with all details  
-        System.out.printf("Processes Burst time Waiting"
-                       +" time Turn around time\n"); 
-  
-        // Calculate total waiting time and total turn  
-        // around time  
-        for (int i = 0; i < n; i++) { 
-            total_wt = total_wt + wt[i]; 
-            total_tat = total_tat + tat[i]; 
-            System.out.printf(" %d ", (i + 1)); 
-            System.out.printf("     %d ", bt[i]); 
-            System.out.printf("     %d", wt[i]); 
-            System.out.printf("     %d\n", tat[i]); 
-        } 
-        float s = (float)total_wt /(float) n; 
-        int t = total_tat / n; 
-        System.out.printf("Average waiting time = %f", s); 
-        System.out.printf("\n"); 
-        System.out.printf("Average turn around time = %d ", t); 
-    } 
-  
-    // Driver code  
-    public static void main(String[] args) throws ParseException { 
-        //process id's  
-        int processes[] = {1, 2, 3}; 
-        int n = processes.length; 
-  
-        //Burst time of all processes  
-        int burst_time[] = {10, 5, 8}; 
-  
-        findavgTime(processes, n, burst_time); 
-  
-    } 
+	
+	private static Process[] processOriginal;
+	private int processnumber;
+	
+	private int totalWaitTime;
+	private int totalTurnaroundTime;
+	
+	public FCFS(int n) {
+		// create process list based on input number (length)
+		processOriginal = new Process[n];
+		createProcessFCFS(n);
+		processnumber = n;
 
+		FCFSProcess();
+	}
+
+	
+	private void FCFSProcess() {
+		waitTime();
+		finishTime();
+		println();
+	}
+  
+	//calculate the wait time for each process
+	private void waitTime() {
+		int temp;
+		totalWaitTime = 0;
+		for (int i = 0; i < processnumber; i++) {
+			temp = 0;
+			if(i != 0) {
+				for (int j = i-1; j >= 0; j--) {
+					temp += processOriginal[j].getCPUBurstList()[0];		
+				}
+			}
+			processOriginal[i].setWaitTime(temp);
+			totalWaitTime += temp;
+		}
+	}
+	
+	//calculate finishTime
+	private void finishTime() {
+		int temp;
+		totalTurnaroundTime = 0;
+		for (int i = 0; i < processnumber; i++) {
+			temp = 0;
+			for (int j = i; j >= 0; j--) {
+				temp += processOriginal[j].getCPUBurstList()[0];
+			}
+			processOriginal[i].setFinishTime(temp);
+			int turnAroundT = temp - processOriginal[i].getStartTime();
+			processOriginal[i].setTurnAroundTime(turnAroundT);
+			totalTurnaroundTime += turnAroundT;
+		}
+	}
+
+	// create random process with id, burst, arrival and priority
+	private static void createProcessFCFS(int number) {
+		for (int i = 0; i < number; i++) {
+			int[] bArray = { (int) (Math.random() * 60 + 1) };
+			processOriginal[i] = new Process(i, bArray);
+		}
+	}
+
+	//print all the information
+	public void println() {
+		for (int i = 0; i < processnumber; i++) {
+			System.out.println("ID: " + processOriginal[i].getPid() + 
+					" Brust: " + processOriginal[i].getCPUBurstList()[0] + 
+					" WaitTime: " + processOriginal[i].getWaitTime() +
+					" TurnAroundTime: " + 
+					(processOriginal[i].getFinishTime() - processOriginal[i].getStartTime())
+					);
+		}
+		
+		double s = ((double)totalWaitTime) / (double)processnumber ;
+		double t = ((double)totalTurnaroundTime) / (double)processnumber;
+		System.out.printf("Average waiting time = %f \n", s); 
+        System.out.printf("Average turn around time = %f \n", t); 
+	}
+  
 }
