18986d8bdb97940b4eefbdb3cd005f6a8014e8c1
Zandermn <dearmrteng@gmail.com>
2021-03-20 17:12:44 -0400

    Revert "Revert "Revert "Update NonpreemptivePriority.java"""
    This reverts commit 76014bafe3e0c0d76bceae8badb88a918f410fca.

--- src/main/NonpreemptivePriority.java
+++ src/main/NonpreemptivePriority.java
@@ -4,20 +4,17 @@ import java.util.*;
 import java.util.Scanner;
 
 public class NonpreemptivePriority {
-	private static Process[]process;
+	private static List<Process> process= new ArrayList<Process>();
 	private int processnumber;
 	private int totalWaitTime;
 	private int totalTurnaroundTime;
-
-	//Constructor
+	
 	public NonpreemptivePriority(int n) {
-		process = new Process[n];
 		createNonpreemptivePriority(n);
 		processnumber = n;
-    NonpreemptivePriorityProcess();
+		NonpreemptivePriorityProcess();
 	}
-
-	//Execute the whole class methods; ComputeFirst-Come,First-Served(FCFS) Scheduling;Compute NonpreemptivePriority Scheduling;
+	
 	private void NonpreemptivePriorityProcess() {
 		waitTime();
 		finishTime();
@@ -28,47 +25,35 @@ public class NonpreemptivePriority {
 		finishTime();
 		println();
 	}
-
-	//Create random process[] with id, burst, arrival and priority
+	
 	private static void createNonpreemptivePriority(int num) {
 		for (int i = 0; i < num; i++) {
 			int[] bArray = { (int) (Math.random() * 60 + 1) };
-			process[i] = new Process(i, bArray);
+			process.add(new Process(i, bArray));
 		}
 	}
-
-	//Set priority for the process[]
+	
 	public void setNonpreemptivePriority() {
-		int[]priority = new int[processnumber];
-		int length = process.length;
+		
+		int size = process.size();
+		int[]priority = new int[size];
 		System.out.println("");
-		System.out.println("Enter the priority for process");
+		System.out.println("Enter the priority for process(start with zero)");
 		Scanner scan=new Scanner(System.in);
-		for(int i=0;i<length;i++) {
+		for(int i=0;i<size;i++) {
 			priority[i]=scan.nextInt();
 		}
 		scan.close();
-
-		int temp1;
-		Process temp2;
-		for(int i=0;i<processnumber-1;i++) {
-			for(int j=0;j<processnumber-1;j++) {
-				if(priority[j]>priority[j+1]) {
-					temp1=priority[j];
-					priority[j]=priority[j+1];
-					priority[j+1]=temp1;
-					
-					temp2 =process[j];
-					process[j]=process[j+1];
-					process[j+1]=temp2;
-					
-				}
-			}
+		
+		List<Process> comp= new ArrayList<Process>();
+		for(int i=0;i<size;i++) {
+			Process temp1=process.get(priority[i]);
+			comp.add(temp1);
 		}
-
+		process = comp;
+		
 	}
-
-	//Compute the wait time and totalWaitTime
+	
 	private void waitTime() {
 		int temp;
 		totalWaitTime = 0;
@@ -76,41 +61,39 @@ public class NonpreemptivePriority {
 			temp = 0;
 			if(i != 0) {
 				for (int j = i-1; j >= 0; j--) {
-					temp += process[j].getCPUBurstList()[0];		
+					temp += process.get(i).getCPUBurstList()[0];		
 				}
 			}
-			process[i].setWaitTime(temp);
+			process.get(i).setWaitTime(temp);
 			totalWaitTime += temp;
 		}
 	}
-
-	//Compute the turn around time and totalTurnaroundTime
+	
 	private void finishTime() {
 		int temp;
 		totalTurnaroundTime = 0;
 		for (int i = 0; i < processnumber; i++) {
 			temp = 0;
 			for (int j = i; j >= 0; j--) {
-				temp += process[j].getCPUBurstList()[0];
+				temp += process.get(j).getCPUBurstList()[0];
 			}
-			process[i].setFinishTime(temp);
-			int turnAroundT = temp - process[i].getStartTime();
-			process[i].setTurnAroundTime(turnAroundT);
+			process.get(i).setFinishTime(temp);
+			int turnAroundT = temp - process.get(i).getStartTime();
+			process.get(i).setTurnAroundTime(turnAroundT);
 			totalTurnaroundTime += turnAroundT;
 		}
 	}
-
-	//Print all the information and compute the average wait time, average turn around time
+	
 	public void println() {
 		for (int i = 0; i < processnumber; i++) {
-			System.out.println("ID: " + process[i].getPid() + 
-					" Brust: " + process[i].getCPUBurstList()[0] + 
-					" WaitTime: " + process[i].getWaitTime() +
+			System.out.println("ID: " + process.get(i).getPid() + 
+					" Brust: " + process.get(i).getCPUBurstList()[0] + 
+					" WaitTime: " + process.get(i).getWaitTime() +
 					" TurnAroundTime: " + 
-					(process[i].getFinishTime() - process[i].getStartTime())
+					(process.get(i).getFinishTime() - process.get(i).getStartTime())
 					);
 		}
-
+		
 		double s = ((double)totalWaitTime) / (double)processnumber ;
 		double t = ((double)totalTurnaroundTime) / (double)processnumber;
 		System.out.printf("Average waiting time = %f \n", s); 
